feb09922619dfd5c6b882a2d9014af6def85714f
rajasekhar <rpasup@wipro.com>
2020-09-09 22:17:29 +0400

    Updated about.js page

--- src/about/about.js
+++ src/about/about.js
@@ -8,19 +8,22 @@ export const About = () => {
   return (
     <Container>
     <div className="products">
-      <h1>Problem Statement!!</h1>
+      <h1>!! Problem Statement !!</h1>
       <p1>For any user, it’s difficult to manage on storing the products information and how many products to be delivered from the ware house in order to send the products to the customer. It will be good to have one website or a page to manage the orders with product list and the basic information (the cost, quantity) stored and displaying the all products list with quantity and prices.</p1>
       <br></br>
-      <p2>Requirement Specification: 
       <br></br>
-      •	User should enter URL on browser and user should be able to see about page of the Product list. <br></br>
+      In this application : 
+      <br></br>
+      <p2>
+      <br></br>
+      •	User should be able to see about Product list information <br></br>
       •	User can add the product with value. <br></br>
       •	User can add the product quantity. <br></br>
       •	User can view the existing product information (Value and Quantity). </p2><br></br><br></br>
       <h1>Team Mates</h1>
       <br></br>
       <Row>
-        <Col xs={6} md={4}>
+        <Col xs={4} md={3}>
         <Card>
           <Card.Img variant="top" src={somdev} />
           <Card.Body>
@@ -28,7 +31,7 @@ export const About = () => {
           </Card.Body>
         </Card>
         </Col>
-        <Col xs={4} md={4}>
+        <Col xs={4} md={3}>
         <Card>
           <Card.Img variant="top" src={rpasup} />
           <Card.Body>
@@ -36,11 +39,11 @@ export const About = () => {
           </Card.Body>
         </Card>
         </Col>
-        <Col xs={6} md={4}>
+        <Col xs={4} md={3}>
         <Card>
           <Card.Img variant="top" src={divya} />
           <Card.Body>
-          <Card.Title>Divyashree Anant Hegde </Card.Title>
+          <Card.Title>Divyashree Hegde </Card.Title>
           </Card.Body>
         </Card>
         </Col>
