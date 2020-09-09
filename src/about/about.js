3e74542d05b369fbc348d8c42d0bc8c8daf7983b
rajasekhar <rpasup@wipro.com>
2020-09-09 20:08:33 +0400

    updated the look and feel

--- src/about/about.js
+++ src/about/about.js
@@ -1,7 +1,9 @@
 import React from "react";
-
+import {Image, Row, Container, Col} from 'react-bootstrap';
+import rpasup from '../rpasup.jpg';
 export const About = () => {
   return (
+    <Container>
     <div className="products">
       <h1>Problem Statement!!</h1>
       <p1>For any user, it’s difficult to manage on storing the products information and how many products to be delivered from the ware house in order to send the products to the customer. It will be good to have one website or a page to manage the orders with product list and the basic information (the cost, quantity) stored and displaying the all products list with quantity and prices.</p1>
@@ -14,9 +16,18 @@ export const About = () => {
       •	User can view the existing product information (Value and Quantity). </p2><br></br><br></br>
       <h1>Team Mates</h1>
       <br></br>
-      1.Rajasekhar <br></br>
-      2.Divya<br></br>
-      3.Sowmya umrila
+      <Row>
+        <Col xs={6} md={4}>
+          <Image src={rpasup} rounded />
+        </Col>
+        <Col xs={4} md={4}>
+          <Image src={rpasup} roundedCircle />
+        </Col>
+        <Col xs={6} md={4}>
+          <Image src={rpasup} thumbnail />
+        </Col>
+      </Row>      
     </div>
+    </Container>
   );
 };
--- src/extrainfo/extrainfo.js
+++ src/extrainfo/extrainfo.js
@@ -1,11 +1,18 @@
 import React from "react";
+import {Image, Container } from 'react-bootstrap';
+import batch23_img from '../Devopstools.png';
 
 export const Extrainfo = () => {
   return (
+
+    <Container>
     <div className="products">
-      <h1>what can be improved !!</h1>
-      1. designs can be better <br></br>
-      2. connect to the data base and get the deliver status 
+        <h1>!! What can be Improved !!</h1>
+        1. Designs can be better <br></br>
+        2. Connect to the data base and get the deliver status  <br></br>
+        3. Below is the tool set we are using for perform complete Devops
     </div>
+    <Image src={batch23_img} rounded />
+    </Container>
   );
 };
