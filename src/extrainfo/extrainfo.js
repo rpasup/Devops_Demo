d8ab2b6d82b9f28bfd797dcf5d82b56ea9ec97ac
rajasekhar <rpasup@wipro.com>
2020-09-09 10:54:52 +0400

    udpated extra info

--- src/extrainfo/extrainfo.js
+++ src/extrainfo/extrainfo.js
@@ -3,21 +3,9 @@ import React from "react";
 export const Extrainfo = () => {
   return (
     <div className="products">
-      <h1>Problem Statement!!</h1>
-      <p1>For any user, it’s difficult to manage on storing the products information and how many products to be delivered from the ware house in order to send the products to the customer. It will be good to have one website or a page to manage the orders with product list and the basic information (the cost, quantity) stored and displaying the all products list with quantity and prices.</p1>
-      <br></br>
-      <p2>Requirement Specification: 
-      <br></br>
-      •	User should enter URL on browser and user should be able to see about page of the Product list. <br></br>
-      •	User can add the product with value. <br></br>
-      •	User can add the product quantity. <br></br>
-      •	User can view the existing product information (Value and Quantity). </p2>
-      <h2>Team</h2>
-      <br></br>
-      we are doing the Capstone project and the team is <br></br>
-      1.Rajasekhar <br></br>
-      2.Divya<br></br>
-      3.Sowmya umrila
+      <h1>what can be improved !!</h1>
+      1. designs can be better <br></br>
+      2. connect to the data base and get the deliver status 
     </div>
   );
 };
