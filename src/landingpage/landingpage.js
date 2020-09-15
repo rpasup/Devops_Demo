4e88fea36834a2bd8a225e4e183f257b1887cbe4
rajasekhar <rpasup@wipro.com>
2020-09-15 13:56:40 +0400

    udpated

--- src/landingpage/landingpage.js
+++ src/landingpage/landingpage.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Container } from 'react-bootstrap';
-// import batch23_img from '../Devopstools.png';
+import { Image, Container } from 'react-bootstrap';
+import devopslifecycle from '../devopslifecycle.jpg';
 
 export const Landingpage = () => {
   return (
@@ -9,6 +9,8 @@ export const Landingpage = () => {
         DevOps is a set of practices that combines software development (Dev) and IT operations (Ops). 
         It aims to shorten the systems development life cycle and provide continuous delivery with high software quality.
         DevOps is complementary with Agile software development; several DevOps aspects came from Agile methodology.
+        <br></br><br></br>
+        <Image src={devopslifecycle} height={450} width={450} style={{ alignSelf: 'center' }}/>
     </Container>
   );
 };
