a4acbabb3a18515f5f661364fe50e1f2dafe4602
rajasekhar <rpasup@wipro.com>
2020-09-17 17:31:14 +0400

    updated with the additional page

--- src/extrainfo/extrainfo.js
+++ src/extrainfo/extrainfo.js
@@ -7,10 +7,8 @@ export const Extrainfo = () => {
 
     <Container>
     <div className="products">
-        <h1>!! What can be Improved !!</h1>
-        1. Designs can be better <br></br>
-        2. Connect to the data base and get the deliver status  <br></br>
-        3. Below is the tool set we are using for perform complete Devops
+        <h1>!! Tools Used !!</h1>
+        * Below is the tool set we are using for performing complete Devops
     </div>
     <Image src={batch23_img} rounded />
     </Container>
