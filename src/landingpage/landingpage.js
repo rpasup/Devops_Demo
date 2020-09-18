0466d64927fe81f3240a5aede6b1c3b663f4a192
rajasekhar <rpasup@wipro.com>
2020-09-18 19:07:51 +0400

    image

--- src/landingpage/landingpage.js
+++ src/landingpage/landingpage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Image, Container } from 'react-bootstrap';
 import devopslifecycle from '../devopslifecycle.jpg';
+import './landling.css';
 
 export const Landingpage = () => {
   return (
@@ -10,7 +11,7 @@ export const Landingpage = () => {
         It aims to shorten the systems development life cycle and provide continuous delivery with high software quality.
         DevOps is complementary with Agile software development; several DevOps aspects came from Agile methodology.
         <br></br><br></br>
-        <Image src={devopslifecycle} height={450} width={1000} style={{ alignSelf: 'center' }}/>
+        <Image src={devopslifecycle} height={'50%'} width={'50%'} className="image" />
     </Container>
   );
 };
