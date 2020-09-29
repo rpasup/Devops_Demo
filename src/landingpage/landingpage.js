dca31de493f79ff610bdeebe0bbeea72ebce790a
rajasekhar <rpasup@wipro.com>
2020-09-29 09:45:33 +0400

    demo of capstone

--- src/landingpage/landingpage.js
+++ src/landingpage/landingpage.js
@@ -7,7 +7,7 @@ export const Landingpage = () => {
   return (
 
     <Container>
-        Demo DevOps is a set of practices that combines software development (Dev) and IT operations (Ops). 
+        DevOps is a set of practices that combines software development (Dev) and IT operations (Ops). 
         It aims to shorten the systems development life cycle and provide continuous delivery with high software quality.
         DevOps is complementary with Agile software development; several DevOps aspects came from Agile methodology.
         <br></br><br></br>
